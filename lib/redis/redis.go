cf1431591c172599453a3badfb45bbcb77b1ab44
jsonlws <jsonlws@163.com>
2022-05-29 23:04:46 +0800

    增加beanstalk处理程序以及redis操作

--- lib/redis/redis.go
+++ lib/redis/redis.go
@@ -9,20 +9,21 @@ import (
 	"github.com/spf13/viper"
 )
 
+var RedisServer *redis.Client
+
 //初始化reids连接对象
-func NewRedis(db int) *redis.Client {
-	RedisObj := redis.NewClient(&redis.Options{
+func NewRedis() {
+	RedisServer = redis.NewClient(&redis.Options{
 		Network:  "tcp",
 		Addr:     fmt.Sprintf("%s:%d", viper.GetString("redis.host"), viper.GetInt("redis.port")),
 		Password: viper.GetString("redis.pwd"),    // redis连接密码
-		DB:       db,                              // 使用redis的库
+		DB:       viper.GetInt("redis.db"),        // 使用redis的库
 		PoolSize: viper.GetInt("redis.pool_size"), // 连接池大小
 	})
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	pong, err := RedisObj.Ping(ctx).Result()
+	pong, err := RedisServer.Ping(ctx).Result()
 	if err != nil {
 		panic(pong)
 	}
-	return RedisObj
 }
